3fd883730117077c8ae8485db1afed6a6a70e43b
Clayton Robbins <robbinsclay@gmail.com>
2021-04-29 09:56:35 -0400

    started signup sheet

--- swim_lessons.py
+++ swim_lessons.py
@@ -11,3 +11,16 @@
 
 ####################READY SET GO!#####################
 
+#Sign up menu
+
+print("First off I would like to thank you for selecting me as your instructor")
+input("Student name: " , name)
+input("student birthday: " , birthday)
+input("Age of student: " , age)
+input("name of parent: ", parent)
+input("Contract number", cont_number)
+print(name)
+print(birthday)
+print(age)
+print(parent)
+
