561f4b51cac152ec51ca92b401f563b767eda380
Clayton Robbins <robbinsclay@gmail.com>
2021-04-29 13:01:54 -0400

    added main menu

--- swim_lessons.py
+++ swim_lessons.py
@@ -19,8 +19,35 @@ input("student birthday: " , birthday)
 input("Age of student: " , age)
 input("name of parent: ", parent)
 input("Contract number", cont_number)
-print(name)
-print(birthday)
-print(age)
-print(parent)
+print("student: " + name)
+print("birthday: " + birthday)
+print("age: " + age)
+print("parent: " + parent)
 
+
+#lesson menu
+
+menu = {}
+
+menu = ["1"] = "Find student"
+menu = ["2"] = "Add student"
+menu = ["3"] = "Archive student"
+menu = ["4"] = "exit"
+
+while True:
+	options = menu.keys()
+	options.sort()
+		for entry in options:
+			print entry, menu[entry]
+
+		selection = raw_input("Please select an option: ")
+		if selection == '1'
+			print("Find student")
+		if selection == '2'
+			print("Add student")
+		if selection == '3'
+			print("Archive student")
+		if selection == '4'
+			print("EXIT")
+		else:
+			print("Unknown selection")
