803b3a9e2b813b4a9e9f77b45a9ae57785aecb56
Clayton Robbins <robbinsclay@gmail.com>
2021-04-30 10:22:03 -0400

    Morning commit. Some changes made. with '#' will be ignored, and an empty message aborts the commit.

--- swim_lessons.py
+++ swim_lessons.py
@@ -13,16 +13,18 @@
 
 #Sign up menu
 
-print("First off I would like to thank you for selecting me as your instructor")
-input("Student name: " , name)
-input("student birthday: " , birthday)
-input("Age of student: " , age)
-input("name of parent: ", parent)
-input("Contract number", cont_number)
-print("student: " + name)
-print("birthday: " + birthday)
-print("age: " + age)
-print("parent: " + parent)
+
+def add_student():
+        print("First off I would like to thank you for selecting me as your instructor")
+        input("Student name: " , name)
+        input("student birthday: " , birthday)
+        input("Age of student: " , age)
+        input("name of parent: ", parent)
+        input("Contract number", cont_number)
+        print("student: " + name)
+        print("birthday: " + birthday)
+        print("age: " + age)
+        print("parent: " + parent)
 
 
 #lesson menu
@@ -35,23 +37,22 @@ menu = ["3"] = "Archive student"
 menu = ["4"] = "exit"
 
 while True:
-	options = menu.keys()
-	options.sort()
-		for entry in options:
-			print entry, menu[entry]
-
-		selection = raw_input("Please select an option: ")
-		if selection == '1'
-			input("Student" , student)
-			find_student(student)
-		if selection == '2'
-			print("Add student")
-			add_student()
-		if selection == '3'
-			print("Archive student")
-			input("Student: " , student
-			archive_student(student)
-		if selection == '4'
-			print("EXIT")
-		else:
-			print("Unknown selection")
+        options = menu.keys()
+        options.sort()
+        for entry in options:
+                        print(entry, menu[entry])
+                        selection = raw_input("Please select an option: ")
+                        if selection == '1':
+                                input("Student" , student)
+                                find_student(student)
+                        elif selection == '2':
+                                print("Add student")
+                                add_student()
+                        elif selection == '3':
+                                print("Archive student")
+                                input("Student: " , student)
+                                archive_student(student)
+                        elif selection == '4':
+                                      print("EXIT")
+                        else:
+                                      print("Unknown selection")
